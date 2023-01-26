6604dcb4ff1723cdf71ad0135f235244c4ce3c3a
thpSachi <122494045+thpSachi@users.noreply.github.com>
2023-01-26 18:51:30 +0100

    Update 02_Mairiechristmas.rb

--- lib/02_Mairiechristmas.rb
+++ lib/02_Mairiechristmas.rb
@@ -2,16 +2,20 @@ require 'nokogiri'
 require 'open-uri'
  
 # 対象のURL
-url = "https://coinmarketcap.com/all/views/all/"
+url = "http://annuaire-des-mairies.com/"
  
 # NokogiriでURLの情報を取得する
-page = Nokogiri::HTML(URI.open(url),nil,"utf-8")
+rootpage = Nokogiri::HTML(URI.open(url),nil,"utf-8")
 
 # Get Symbol
-arr_symbol = [] 
-page.xpath('//a[contains(@class, "cmc-table__column-name--symbol")]').each do |link|
-    arr_symbol.push(link.content)
+arr_region = [] 
+rootpage.xpath('//td/a').each do |link|
+    #arr_symbol.push(link.content)
+    arr_region.push(link['href'])
+    puts arr_region
 end
+
+page = Nokogiri::HTML(URI.open(url),nil,"utf-8")
 
 # Get Price
 arr_price = [] 
@@ -20,5 +24,5 @@ page.xpath('//a[contains(@class, "cmc-link")]/span').each do |link|
 end
 
 # Make Hash
-resule_hash = Hash[arr_symbol.zip arr_price.map]
-puts resule_hash
+#resule_hash = Hash[arr_symbol.zip arr_price.map]
+#puts resule_hash
